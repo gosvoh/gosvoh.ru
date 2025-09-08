e0d321ba3079b820f398f2b1b4fd6e2ae0675811
Aleksei Vokhmin <vohmina2011@yandex.ru>
2025-09-08 11:23:59 +0300

    Update tailwind to v4

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import { visualizer } from "rollup-plugin-visualizer";
 import path from "path";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), visualizer()],
+  plugins: [react(), tailwindcss(), visualizer()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
