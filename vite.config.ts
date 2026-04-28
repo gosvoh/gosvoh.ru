abdce6c103d36b9fc060aa933ec072425a654cd7
Aleksey Vokhmin <vohmina2011@yandex.ru>
2026-04-28 19:22:11 +0300

    Update dependencies and devDependencies in package.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import { visualizer } from "rollup-plugin-visualizer";
 import path from "path";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), visualizer()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
